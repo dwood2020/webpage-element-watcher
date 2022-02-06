071822589671fe9c406a990748f273e47211ba3d
David <d.woodward@gmx.de>
2022-02-06 19:13:08 +0100

    introduced jobresult class

--- Job.cs
+++ Job.cs
@@ -4,9 +4,33 @@ using HtmlAgilityPack;
 namespace Watcher {
 
     /// <summary>
+    /// This class represents a Job result which consists of a job execution timestamp and the result value.
+    /// </summary>
+    /// <typeparam name="T">Result type: string (default) or double (called "number")</typeparam>
+    public class JobResult<T> {
+
+        public string Timestamp { get; set; }
+        public T Content { get; set; }
+
+        public JobResult(string timestamp, T content) {
+            Timestamp = timestamp;
+            Content = content;
+        }
+
+        public override string ToString() {
+            return "Timestamp: " + Timestamp + "  Content: " + Content;
+        }
+    }
+
+
+    /// <summary>
     /// This class represents a watcher job.
     /// </summary>
     public class Job {
+
+        // NOTE: These simple strings are used instead of a proper enum to make TOML parsing easy and automatic
+        public static readonly string ResultTypeNumber = "number";
+        public static readonly string ResultTypeString = "string";
 
         /// <summary>
         /// Descriptive name of the Job. Used for debugging/logging purposes.
@@ -23,11 +47,21 @@ namespace Watcher {
         /// </summary>
         public string Xpath { get; set; } = String.Empty;
 
+        public string ResultType { get; set; } = ResultTypeString;
+
         /// <summary>
-        /// The job result: Inner HTML of the XPath Query result.
+        /// The job result: Inner HTML of the XPath Query result as String.
+        /// (This is the default)
         /// Set to Null if no result found.
         /// </summary>
-        public string? Result { get; private set; }
+        public JobResult<string>? StringResult { get; private set; }
+
+        /// <summary>
+        /// The job result: Inner HTML of the XPath Query result as Number(double).
+        /// (This is the default)
+        /// Set to Null if no result found.
+        /// </summary>
+        public JobResult<double>? NumberResult { get; private set; }
 
         // every Job has its own HTML document as they can be generally different in each job
         private readonly HtmlDocument htmlDoc;
@@ -37,6 +71,8 @@ namespace Watcher {
 
         public Job() {
             htmlDoc = new HtmlDocument();   //TODO: Check if it is ok to use 1 instance for each job Run
+            StringResult = null;
+            NumberResult = null;
         }
 
         // This ctor should actually be used if my design was better
@@ -59,7 +95,8 @@ namespace Watcher {
         public async Task Run() {
 
             // set Result to null on every run first
-            Result = null;
+            StringResult = null;
+            NumberResult = null;
 
             // this can be done in async method, see
             // here: https://stackoverflow.com/questions/25055749/terminate-or-exit-c-sharp-async-method-with-return
@@ -84,8 +121,23 @@ namespace Watcher {
                 logger?.Warning("Job {0}: Using first node. This may be incorrect", Name);
             }
 
-            Result = nodes[0].InnerHtml;
-            logger?.XpathQueryResult(Name, Result);            
+            // timestamp of job execution
+            string timestamp = DateTime.Now.ToString("G");
+
+            if (ResultType == ResultTypeNumber) {
+                double parseResult = 0.0;
+                if (!Double.TryParse(nodes[0].InnerText, out parseResult)) {
+                    logger?.Error("Job {0}: Number result type could not be parsed. Setting 0", Name);
+                }
+                NumberResult = new JobResult<double>(timestamp, parseResult);
+                logger?.XpathQueryResult(Name, NumberResult.ToString());
+            }
+            else {
+                StringResult = new JobResult<string>(timestamp, nodes[0].InnerText);
+                logger?.XpathQueryResult(Name, StringResult.ToString());
+            }
+            
+            
         }
         
     }
